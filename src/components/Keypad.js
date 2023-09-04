2479ce064e98437be4634c3a6d648ee67ddf3a5b
Benarba Tewfik <tewfikhotspur@gmail.com>
2023-09-04 07:06:22 +0200

    Completed assignment

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,14 @@
 // Code Keypad Component Here
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+function Keypad() {
+  function handelChange() {
+    console.log("Entering password...");
+  }
+  return (
+    <div>
+      <input type="password" onChange={handelChange} />
+    </div>
+  );
 }
 
 export default Keypad;
